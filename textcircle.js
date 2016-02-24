0b45974b1484f60eaf82bc2d967e7122de979aa1
Goh Zhao Yang <gzy_dark@hotmail.com>
2016-02-24 18:56:12 +0800

    Textcircle-1st lesson

---
+++ textcircle.js
@@ -0,0 +1,28 @@
+this.Documents = new Mongo.Collection("documents");
+
+
+if (Meteor.isClient) {
+  Template.editor.helpers({
+      docid:function(){
+          
+          var doc = Documents.findOne();
+          if (doc){
+              return doc._id;
+          }
+          else{
+              return undefined;
+          }
+      }
+  });
+
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+      if (!Documents.findOne()){
+          Documents.insert({title:"My new documents"});
+      }
+  });
+}
