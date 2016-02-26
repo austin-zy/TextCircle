8efe2e193aedacbff48ad7a61999a3c75ea4ce16
Goh Zhao Yang <gzy_dark@hotmail.com>
2016-02-27 00:36:05 +0800

    html-preview

--- textcircle.js
+++ textcircle.js
@@ -25,10 +25,16 @@ if (Meteor.isClient) {
           else{
               return undefined;
           }
-      }
+      },
+      config:function(){
+        return function(editor){
+            editor.on("change", function(cm_editor, info){
+                console.log(cm_editor.getValue());
+                $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+            });
+            }
+        },
   });
-
-
 }
 
 if (Meteor.isServer) {
